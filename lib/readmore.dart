55176926765e41f93ffe41e4cd64a7bf59d33bef
SchnMar <hello@marcelschneider.eu>
2020-07-19 10:26:10 +0200

    removed print statement

--- lib/readmore.dart
+++ lib/readmore.dart
@@ -118,8 +118,6 @@ class ReadMoreTextState extends State<ReadMoreText> {
         textPainter.layout(minWidth: constraints.minWidth, maxWidth: maxWidth);
         final textSize = textPainter.size;
 
-        print('linkSize $linkSize textSize $textSize');
-
         // Get the endIndex of data
         bool linkLongerThanLine = false;
         int endIndex;
